8375679ff1251a33acb034ac8f982474092f7e3a
Emmanuel Bastien <os@ebastien.name>
2015-04-14 10:41:30 +0200

    Rename application module.

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "action_controller/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SQLegalize
+module TiDredger
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
