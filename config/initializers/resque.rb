3786da6be05bddebfc7f33171dce5cd7d7484060
Emmanuel Bastien <os@ebastien.name>
2015-04-14 11:27:41 +0200

    Do not initialize Redis connection in test environment.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,10 +6,12 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 resque_env = resque_config[rails_env]
 
-begin
-  Resque.redis = Redis.new(url: "redis://#{resque_env['redis']}")
-  Resque.redis.ping
-rescue Redis::CannotConnectError => e
-  Rails.logger.fatal "Cannot connect to Redis server: #{e.message}"
-  raise e
+unless Rails.env.test?
+  begin
+    Resque.redis = Redis.new(url: "redis://#{resque_env['redis']}")
+    Resque.redis.ping
+  rescue Redis::CannotConnectError => e
+    Rails.logger.fatal "Cannot connect to Redis server: #{e.message}"
+    raise e
+  end
 end
